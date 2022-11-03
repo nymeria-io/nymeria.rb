b19ad4efc1b96d4fac04a3fd3ede91af70652ecc
Corey Prophitt <git@prophitt.me>
2022-11-03 15:33:06 -0700

    Moved constants into module

--- lib/nymeria.rb
+++ lib/nymeria.rb
@@ -7,12 +7,12 @@ require 'nymeria/person'
 require 'json'
 require 'net/http'
 
-API_KEY = ''
-BASE_URL = 'https://www.nymeria.io/api/v4'
-USER_AGENT = 'nymeria.rb/2.0.6'
-
 # Nymeria is our primary module namespace.
 module Nymeria
+  API_KEY = ''
+  BASE_URL = 'https://www.nymeria.io/api/v4'
+  USER_AGENT = 'nymeria.rb/2.0.8'
+
   class << self
     def request(req)
       req['X-Api-Key'] = API_KEY
