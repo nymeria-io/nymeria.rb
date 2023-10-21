15ade490c2b985da39e88ef5bbaae852e087b3ed
Corey Prophitt <git@prophitt.me>
2023-10-21 09:06:10 -0700

    Don't set a default const API_KEY value

--- lib/nymeria.rb
+++ lib/nymeria.rb
@@ -9,13 +9,12 @@ require 'net/http'
 
 # Nymeria is our primary module namespace.
 module Nymeria
-  API_KEY = ''
   BASE_URL = 'https://www.nymeria.io/api/v4'
   USER_AGENT = 'nymeria.rb/2.0.8'
 
   class << self
     def request(req)
-      req['X-Api-Key'] = API_KEY
+      req['X-Api-Key'] = defined?(API_KEY) ? API_KEY : ''
       req['User-Agent'] = USER_AGENT
       req
     end
